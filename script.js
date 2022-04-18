8609e4bf712b786cc349a533186dea90dadbe9f0
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2022-04-18 19:06:51 +0400

    Form & new color
    Added search form for navbar and new info color for buttons, etc.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 var navBurger = document.querySelector('.nav-burger');
 var navLinks = document.querySelector('.nav-links');
-var navOverlay = document.querySelector('.nav-overlay');
 
 navBurger.addEventListener('click', ()=>{
     navBurger.classList.toggle('nav-burger-active');
     navLinks.classList.toggle('nav-links-active');
-    navOverlay.classList.toggle('nav-overlay-active');
     document.body.classList.toggle('overflow-hidden');
 });
